ccb399a4f14f8658e4f923eda2d0f1752343589a
Nellie McKesson <nelliemckesson@gmail.com>
2017-05-02 12:55:04 -0400

    updating paths

--- wordmaker.rb
+++ wordmaker.rb
@@ -6,7 +6,7 @@ inputfile = File.expand_path(unescapeargv)
 inputfile = inputfile.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).join(File::SEPARATOR)
 filename_split = inputfile.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop
 filename = inputfile.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact)).pop.rpartition('.').first.gsub(/ /, "")
-working_dir = inputfile.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact))[0...-2].join(File::SEPARATOR)
+working_dir = inputfile.split(Regexp.union(*[File::SEPARATOR, File::ALT_SEPARATOR].compact))[0...-1].join(File::SEPARATOR)
 final_dir = File.join(working_dir, filename)
 final_file = File.join(final_dir, filename_split)
 filetype = inputfile.split(".").pop
@@ -49,19 +49,15 @@ ensure
   puts logstring
 end
 
-def localRunNode(jsfile, args)
+def localRunNode(jsfile, args, os, resource_dir)
   if os == "mac" or os == "unix"
     `node #{js} #{args}`
   elsif os == "windows"
     nodepath = File.join(resource_dir, "nodejs", "node.exe")
     `#{nodepath} #{js} #{args}`
   else
-    File.open(Bkmkr::Paths.log_file, 'a+') do |f|
-      f.puts "----- NODE ERROR"
-      f.puts "ERROR: I can't seem to run node. Is it installed and part of your system PATH?"
-      f.puts "ABORTING. All following processes will fail."
+    puts "Can't run node!"
     end
-    File.delete(Project.alert)
   end
 rescue => logstring
 ensure
@@ -95,7 +91,7 @@ makeDir(final_dir)
 moveFile(inputfile, final_dir)
 
 # prep the html file for conversion
-localRunNode(htmlconversionsjs, final_file)
+localRunNode(htmlconversionsjs, final_file, os, resource_dir)
 
 # convert .html to .docx via powershell script
 convertHMTLToDocxPSscript(savehtmlasdocxps, filetype, final_file, outputfile)
