7404be17b2f40353cdc779e6e33d7007b670a01d
Nellie McKesson <nelliemckesson@gmail.com>
2017-05-02 13:47:55 -0400

    fixing remote deploy

--- remotedeploy.bat
+++ remotedeploy.bat
@@ -1,2 +1,4 @@
-cd S:\resources\bookmaker_scripts\wordmaker
-git pull origin master
+set logfile="C:\Users\padwoadmin\Dropbox (Macmillan Publishers)\wordmaker_stg\stdout-and-err.txt"
+
+cd S:\resources\bookmaker_scripts\wordmaker >> %logfile% 2>&1
+git pull origin master >> %logfile% 2>&1
