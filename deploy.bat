4d981bac104dbf6b4e07f972ba6c6775a79ee8f9
Nellie McKesson <nelliemckesson@gmail.com>
2017-05-02 12:47:36 -0400

    updating deploy to log

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,3 @@
-C:\Ruby200\bin\ruby.exe S:\resources\bookmaker_scripts\wordmaker\wordmaker.rb '%1'
+set logfile="C:\Users\padwoadmin\Dropbox (Macmillan Publishers)\wordmaker_stg\stdout-and-err.txt"
+
+C:\Ruby200\bin\ruby.exe S:\resources\bookmaker_scripts\wordmaker\wordmaker.rb '%1' >> %logfile% 2>&1
