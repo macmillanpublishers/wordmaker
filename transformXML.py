f36c3d23362abf7179029cd71102516841a607d7
nelliemckesson <nelliemckesson@gmail.com>
2017-05-05 11:21:22 -0400

    writing python etree handling

--- transformXML.py
+++ transformXML.py
@@ -1,12 +1,30 @@
 from sys import argv
+from lxml import etree
 
 filename = argv[1]
 
 import os
-import zipfile
 import shutil
 import re
-import xml.etree.ElementTree as ET
+import uuid
+
+ns = {'w' : 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+
+def generate_textid(counter):
+    idbase = uuid.uuid4().hex
+    idshort = idbase[:8]
+    iduniq = idshort + str(counter)
+    iduniq = iduniq[-8:]
+
+    return str(iduniq)
+
+def generate_rsid(counter):
+    idbase = uuid.uuid4().hex
+    idshort = idbase[:8]
+    iduniq = idshort + str(counter)
+    iduniq = "00" + iduniq[-6:]
+
+    return str(iduniq)
 
 def convert_footnotes(self):
     # https://docs.python.org/2/library/xml.etree.elementtree.html
@@ -14,9 +32,23 @@ def convert_footnotes(self):
     # grab and parse each footnote object
     # create new object and remap to required footnote formatting
     # add to footnote.xml file
-    tree = ET.parse('document.xml')
+    footnotecounter = 1
+
+    tree = etree.parse(self)
     root = tree.getroot()
+    print(root.tag)
+    for para in root.findall(".//{http://schemas.openxmlformats.org/wordprocessingml/2006/main}pStyle[@{http://schemas.openxmlformats.org/wordprocessingml/2006/main}val='footnotetext']") :
+      myparent = para.getparent().getparent()
+      textid = generate_textid(footnotecounter)
+      rsid = generate_rsid(footnotecounter)
+      myparent.attrib['{http://schemas.microsoft.com/office/word/2010/wordml}paraId'] = textid
+      myparent.attrib['{http://schemas.microsoft.com/office/word/2010/wordml}textId'] = '77777777'
+      myparent.attrib['{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rsidR'] = rsid
+      myparent.attrib['{http://schemas.openxmlformats.org/wordprocessingml/2006/main}rsidRDefault'] = rsid
+      parentstring = etree.tostring(myparent)
+      footnotecounter += 1
+      print parentstring
 
     return
 
-convert_manuscript( filename )
+convert_footnotes( filename )
